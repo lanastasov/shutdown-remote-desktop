1786766985c6f96439f15a7150e05a8c961418f7
lanastasov <lubomir.anastasov@gmail.com>
2020-10-17 16:46:22 +0300

    configure remote shutdown

--- shutremote.go
+++ shutremote.go
@@ -5,6 +5,7 @@ import (
 	"os/exec"
 )
 
+// https://github.com/lanastasov/windows_automation/blob/master/cmd-prompt/remote-shutdown-local-network.png
 func main() {
 	// shutdown -r -m \\desktop-7huo5h1 -t 01
 	cmd, err := exec.Command("shutdown", "/s", "/m", "\\\\desktop-7huo5h1", "/t", "01").Output()
