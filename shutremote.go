ea60a5397f94a377ba702ea01720f383e14de23a
lanastasov <lubomir.anastasov@gmail.com>
2020-12-19 20:23:53 +0200

    adding second host

--- shutremote.go
+++ shutremote.go
@@ -7,7 +7,12 @@ import (
 
 // https://github.com/lanastasov/windows_automation/blob/master/cmd-prompt/remote-shutdown-local-network.png
 func main() {
-	// shutdown -r -m \\desktop-7huo5h1 -t 01
+	// shutremote 1 - execute first command
+	// shutremote 2 - execute second command
+	// shutremtoe 3 - executes first and second command
+	
+	// shutdown -s -m \\desktop-7huo5h1 -t 01
+	// shutdown -s -m 192.168.1.14 -t 01
 	cmd, err := exec.Command("shutdown", "/s", "/m", "\\\\desktop-7huo5h1", "/t", "01").Output()
 	if err != nil {
 		fmt.Println("err = ", err)
